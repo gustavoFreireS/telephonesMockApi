488fa95059ac13e330dc8fc43c67190997c046e7
Gustavo Freire Santos <gustavofreiresantos@Gustavos-MacBook-Pro.local>
2018-07-28 17:09:28 -0300

    set max number

--- config/apiMock.js
+++ config/apiMock.js
@@ -1,20 +1,22 @@
 var populateApi = function(page, perPage) {
   var temp = [];
   var i;
-  var number = ((page - 1)*perPage) + 555000000;
+  var number = ((page - 1) * perPage) + 555000000;
   for (i = 0; i < perPage; i++) {
-    let teltemp = {
-      number: number,
-      cost: (parseInt(number.toString().substr(-2)) + 100) / 100
-    };
-    number++;
-    temp.push(teltemp);
+    if (number <= 555000999) {
+      let teltemp = {
+        number: number,
+        cost: (parseInt(number.toString().substr(-2)) + 100) / 100,
+      };
+      number++;
+      temp.push(teltemp);
+    }
   }
   let telephones = new Object;
   telephones.meta = {
     page: page,
     perPage: perPage,
-    totalPages: Math.ceil(1000/perPage),
+    totalPages: Math.ceil(1000 / perPage)
   };
   telephones.data = temp;
   return telephones;
